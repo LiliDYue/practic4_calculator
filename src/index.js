52873e8ae7616619f717fb25bc826260793a55e5
¨lilia¨ <alejandraph@iteso.mx>
2026-04-27 00:13:06 -0600

    prueba integrate workflow2

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ const app = require('./app');
 
 app.listen(3000, () => {
   console.log('prueba para integrate');
+  console.log('prueba para integrate2');
   console.log('Servidor corriendo en puerto 3000');
 });
